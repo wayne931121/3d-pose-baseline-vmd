8b7a978e75c06c32ab7faad44d2a34be2c3a9650
miu <garnet200521358@gmail.com>
2019-01-13 15:50:31 +0900

    デバッグ粒度のパラメータ指定修正

--- src/openpose_3dpose_sandbox_vmd.py
+++ src/openpose_3dpose_sandbox_vmd.py
@@ -70,7 +70,7 @@ def main(_):
     start_frame_f = open(subdir +'/start_frame.txt', 'w')
 
     idx = FLAGS.person_idx - 1
-    start_frame_index, smoothed = openpose_utils.read_openpose_json(openpose_output_dir, idx, level[FLAGS.verbose] == 3)
+    start_frame_index, smoothed = openpose_utils.read_openpose_json(openpose_output_dir, idx, FLAGS.verbose == 3)
 
     # 開始フレームインデックスを保存
     start_frame_f.write(str(start_frame_index))
