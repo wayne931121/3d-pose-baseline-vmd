2c2c646d67ff734e8b37afb4c9300336dc092cd8
miu <garnet200521358@gmail.com>
2019-06-01 19:53:02 +0900

    3Dでのaspect指定コメントアウト

--- src/viz.py
+++ src/viz.py
@@ -57,7 +57,8 @@ def show3Dpose(channels, ax, lcolor="#3498db", rcolor="#e74c3c", add_labels=Fals
   ax.get_xaxis().set_ticklabels([])
   ax.get_yaxis().set_ticklabels([])
   ax.set_zticklabels([])
-  ax.set_aspect('equal')
+  # 3.1.0で3Dには指定不可(6/1)
+  # ax.set_aspect('equal')
 
   # Get rid of the panes (actually, make them white)
   white = (1.0, 1.0, 0.1, 0.0)
